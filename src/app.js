a7ab9027fc5586bc8953a273a682f72b8cfdbc57
kalyan <mkalyan8400@gmail.com>
2025-09-17 19:26:14 +0530

    added whataspp route for my project

--- src/app.js
+++ src/app.js
@@ -6,4 +6,14 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/json", jsonDataRouter);
+
+app.post("/whatsapp/reply", (req, res) => {
+  const from = req.body.From; // sender's WhatsApp number
+  const body = req.body.Body; // message text
+
+  console.log(`📩 Message received from ${from}: ${body}`);
+
+  // Respond with 200 OK (Twilio expects a response)
+  res.send("Message received ✅");
+});
 export default app;
